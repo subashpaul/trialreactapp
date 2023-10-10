f534bfff321c499289c856b321ddd97c18207a65
subashpaul <subashpaul559@gmail.com>
2023-10-10 12:28:09 +0530

    New commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Automovill Tech Pvt. Ltd.
+          Automovill Tech Pvt. Ltd. App
         </p>
         <a
           className="App-link"
